c7d4c85ae33b67da1f6b71d43f988ac09c6e4fcc
ITF_PC <gshee@naver.com>
2023-07-18 15:17:57 +0900

    1. A개발자가 B에게 주석을 날리기위해 작성함.

--- app/src/main/java/com/example/githubexam/MainActivity.java
+++ app/src/main/java/com/example/githubexam/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //A 개발자가 B에게 푸쉬를 했다
     }
 }
